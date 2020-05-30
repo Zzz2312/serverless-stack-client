8d9a755c38dbf04d6b7d782b3ee4172f8bb5db9d
Zzz2312 <zarnihein2312@gmail.com>
2020-05-30 17:32:21 +0630

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1", 
+        BUCKET: "z-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://gatow4m7ki.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_RQY8OLe2r",
+        APP_CLIENT_ID: "3hu3f9mkpffv35gn4dc92d6r5j",
+        IDENTITY_POOL_ID: "us-east-1:e81682f9-67a3-41ce-8466-2a446cd718ba" 
+    }
+};
