2f76ccb2013c42bb22f65aeb671d9a24986da4ab
Zzz2312 <zarnihein2312@gmail.com>
2020-06-02 22:43:45 +0630

    Building our React app

--- src/config.js
+++ src/config.js
@@ -12,5 +12,7 @@ export default {
         USER_POOL_ID: "us-east-1_RQY8OLe2r",
         APP_CLIENT_ID: "3hu3f9mkpffv35gn4dc92d6r5j",
         IDENTITY_POOL_ID: "us-east-1:e81682f9-67a3-41ce-8466-2a446cd718ba" 
-    }
+    },
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_rzjPlWqEo5CPq5T4hICDH9ZK00hI5GoVSF"
 };
