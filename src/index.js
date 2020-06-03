d5105475968244422f349c0fd0e92cdc61fcab8c
Zzz2312 <zarnihein2312@gmail.com>
2020-06-04 02:31:25 +0630

    Adding a Netlify build script for z-notes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import config from './config';
-import { initSentry } from './libs/errorLib';
 
-initSentry();
 
 Amplify.configure({
   Auth: {
