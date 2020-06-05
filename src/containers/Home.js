a434ed79bc112fef9b8a7a368ee8e6334c9ba359
Zzz2312 <zarnihein2312@gmail.com>
2020-06-05 22:16:38 +0630

    Update Home lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -60,6 +60,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
